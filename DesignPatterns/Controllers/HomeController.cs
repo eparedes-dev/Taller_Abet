0c567f18fbcf2aa9877f4f5edef98bce019182aa
eparedes-dev <erick.paredes.nunez@udla.edu.ec>
2023-07-16 23:12:05 -0500

    Implementacion de Modle Builder

--- DesignPatterns/Controllers/HomeController.cs
+++ DesignPatterns/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using DesignPatterns.Models;
+﻿using DesignPatterns.ModelBuilder;
+using DesignPatterns.Models;
 using DesignPatterns.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -35,14 +36,16 @@ namespace DesignPatterns.Controllers
         [HttpGet]
         public IActionResult AddMustang()
         {
-            _vehicleRepository.AddVehicle(new Car("red","Ford","Mustang"));
+            var builder = new CarBuilder();
+            _vehicleRepository.AddVehicle(builder.Build());
             return Redirect("/");
         }
 
         [HttpGet]
         public IActionResult AddExplorer()
         {
-            _vehicleRepository.AddVehicle(new Car("red", "Ford", "Explorer"));
+            var builder = new CarBuilder();
+            _vehicleRepository.AddVehicle(builder.SetModel("Explorer").Build());
             return Redirect("/");
         }
 
---
+++ DesignPatterns/ModelBuilder/CarBuilder.cs
@@ -0,0 +1,36 @@
+﻿using DesignPatterns.Models;
+using System.Runtime.CompilerServices;
+
+namespace DesignPatterns.ModelBuilder
+{
+    public class CarBuilder
+    {
+        public string Brand = "Ford";
+        public string Model = "Mustang";
+        public string Color = "Red";
+
+        public CarBuilder SetBrand(string brand)
+        {
+            Brand = brand;
+            return this;
+        }
+
+        public CarBuilder SetModel(string model)
+        {
+            Model = model;
+            return this;
+        }
+
+        public CarBuilder SetColor(string color)
+        {
+            Color = color;
+            return this;
+        }
+
+
+        public Car Build()
+        {
+            return new Car(Color, Brand, Model);
+        }
+    }
+}
