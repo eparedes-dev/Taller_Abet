72122807613b990188ccde051346cb900c98ebcf
eparedes-dev <erick.paredes.nunez@udla.edu.ec>
2023-07-16 23:01:18 -0500

    Aplicacion de Singleton

---
+++ DesignPatterns/Infraestructure/Singleton/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using DesignPatterns.Models;
+using System.Collections.Generic;
+
+namespace DesignPatterns.Infraestructure.Singleton
+{
+    public class MemoryCollection
+    {
+        public ICollection<Vehicle> Vehiculos {get; set;}
+
+        private static MemoryCollection Instance;
+
+        public MemoryCollection()
+        {
+            Vehiculos = new List<Vehicle>(); 
+        }
+        public static MemoryCollection GetInstance()
+        {
+            if (Instance == null)
+            {
+                Instance = new MemoryCollection();
+            }
+            return Instance;
+        }
+
+    }
+}
--- DesignPatterns/Repositories/MyVehiclesRepository.cs
+++ DesignPatterns/Repositories/MyVehiclesRepository.cs
@@ -1,4 +1,5 @@
-﻿using DesignPatterns.Models;
+﻿using DesignPatterns.Infraestructure.Singleton;
+using DesignPatterns.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,26 +9,26 @@ namespace DesignPatterns.Repositories
 {
     public class MyVehiclesRepository : IVehicleRepository
     {
-        private readonly ICollection<Vehicle> _memoryCollection;
+        private readonly MemoryCollection _memoryCollection;
 
         public MyVehiclesRepository()
         {
-            _memoryCollection = new List<Vehicle>();
+            _memoryCollection = MemoryCollection.GetInstance();
         }
 
         public void AddVehicle(Vehicle vehicle)
         {
-            _memoryCollection.Add(vehicle);
+            _memoryCollection.Vehiculos.Add(vehicle);
         }
 
         public Vehicle Find(string id)
         {
-           return  _memoryCollection.FirstOrDefault(v => v.ID.Equals(new Guid(id)));
+           return  _memoryCollection.Vehiculos.FirstOrDefault(v => v.ID.Equals(new Guid(id)));
         }
 
         public ICollection<Vehicle> GetVehicles()
         {
-            return _memoryCollection;
+            return _memoryCollection.Vehiculos;
         }
 
         
